812400802d43415bc2d725a8b8550592f6b13b8f
deenathayalan248 <deenathayalan248@gmail.com>
2021-09-14 00:22:04 -0400

    Dummy Test

---
+++ src/test/java/com/nopcommerce/testCases/Dummy_test.java
@@ -0,0 +1,15 @@
+package com.nopcommerce.testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Dummy_test {
+	
+	@Test
+	public void testing123() {
+		Assert.assertTrue(true);
+		
+	}
+
+}
